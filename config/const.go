7825644bd14bd13cd3845046f6a0d0e27e639d6e
larryannon <143927678+larryannon@users.noreply.github.com>
2025-07-10 15:24:40 +0800

    add profiler (#7)

--- config/const.go
+++ config/const.go
@@ -1,11 +1,20 @@
 package config
 
 const (
+	KeyEnv = "env"
+
 	// server
 
 	KeyPprofAddr   = "server.pprof-addr"
 	KeyPprofToken  = "server.pprof-token" //nolint:gosec
 	KeyStopTimeout = "server.stop-timeout"
+
+	// profiler
+
+	KeyProfilerServerAddress = "profiler.server-address"
+	KeyProfilerAuthUsername  = "profiler.auth-username"
+	KeyProfilerAuthPassword  = "profiler.auth-password" //nolint:gosec
+	KeyProfilerDebug         = "profiler.debug"
 
 	// log
 
